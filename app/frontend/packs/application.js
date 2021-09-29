186a30f3d07fdee3628deac2613cd59ded6d5e35
axmaxon <axmaxon@gmail.com>
2021-09-29 13:33:29 +0500

    add: game js script | change: app style

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -2,5 +2,6 @@ import Rails from "@rails/ujs"
 Rails.start()
 import '../styles/application.scss'
 import 'bootstrap/dist/js/bootstrap.js'
+import '../scripts/game.js'
 
 const images = require.context('../images', true)
