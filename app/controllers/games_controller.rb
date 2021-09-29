62c678468298dcfabcdac39e09f8a2c1d03485d6
axmaxon <axmaxon@gmail.com>
2021-09-29 13:03:16 +0500

    add: games#play action

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -17,5 +17,20 @@ class GamesController < ApplicationController
   end
 
   def play
+    game = Game.find(params[:id])
+    game.play!(params[:letter])
+    game.save
+
+    respond_to do |format|
+      format.json do
+        render json: {
+          current: render_to_string(
+            partial: 'games/current',
+            locals: { game: game },
+            formats: [:html]
+          )
+        }
+      end
+    end
   end
 end
