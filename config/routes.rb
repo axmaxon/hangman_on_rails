5c102f027c3d3924bb3d3c7372864674fa0f322a
maxim <axmaxon@gmail.com>
2021-11-04 13:11:13 +0500

    add: routes for words

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :games, only: %i[create show index] do
     post :play, on: :member
   end
+  resources :words, only: %i[create index]
 
   root to: 'games#index'
 end
