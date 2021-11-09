f02a55738722d61b06420a9a2285a2e0a39b6869
maxim <axmaxon@gmail.com>
2021-11-09 16:19:48 +0500

    add: route to delete words

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :games, only: %i[create show index] do
     post :play, on: :member
   end
-  resources :words, only: %i[create index]
+  resources :words, only: %i[create index destroy]
 
   root to: 'games#index'
 end
